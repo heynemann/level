21a1d053362e944ad8ba2e49bf4099e9802f2126
Bernardo Heynemann <heynemann@gmail.com>
2016-07-05 22:52:04 -0300

    Session loading, serialization

--- extensions/session_manager.go
+++ extensions/session_manager.go
@@ -12,7 +12,41 @@ import (
 	"time"
 
 	"gopkg.in/redis.v4"
+	"gopkg.in/vmihailenco/msgpack.v2"
 )
+
+//Session represents an user session
+type Session struct {
+	ID      string
+	Manager *SessionManager
+	data    map[string]interface{}
+}
+
+//Serialize dehidrates an item to the session storage
+func (session *Session) Serialize(payload interface{}) (string, error) {
+	item, err := msgpack.Marshal(payload)
+	if err != nil {
+		return "", err
+	}
+
+	return string(item), nil
+}
+
+//Deserialize rehidrates an item in the session storage
+func (session *Session) Deserialize(payload string) (interface{}, error) {
+	var item interface{}
+	err := msgpack.Unmarshal([]byte(payload), &item)
+	if err != nil {
+		return nil, err
+	}
+
+	return item, nil
+}
+
+//Get returns an item in the session
+func (session *Session) Get(key string) interface{} {
+	return session.data[key]
+}
 
 //SessionNotFoundError identified an error that occurred because a given session was not found.
 type SessionNotFoundError struct {
@@ -26,6 +60,10 @@ func (s *SessionNotFoundError) Error() string {
 // SessionManager is responsible for handling session data
 type SessionManager struct {
 	client *redis.Client
+}
+
+func getSessionKey(sessionID string) string {
+	return fmt.Sprintf("session-%s", sessionID)
 }
 
 //GetSessionManager returns a connected SessionManager ready to be used.
@@ -48,15 +86,15 @@ func GetSessionManager(redisHost string, redisPort int, redisPass string, redisD
 
 //Start starts a new session in the storage (or resumes an old one)
 func (s *SessionManager) Start(sessionID string) {
-	hashKey := fmt.Sprintf("session-%s", sessionID)
+	hashKey := getSessionKey(sessionID)
 	timestamp := fmt.Sprintf("%d", time.Now().UnixNano())
 	s.client.HSet(hashKey, "lastupdated", timestamp)
 }
 
 //Merge gets all the keys from old session into new session (no overwrites done).
 func (s *SessionManager) Merge(oldSessionID, sessionID string) (int, error) {
-	oldHashKey := fmt.Sprintf("session-%s", oldSessionID)
-	hashKey := fmt.Sprintf("session-%s", sessionID)
+	oldHashKey := getSessionKey(oldSessionID)
+	hashKey := getSessionKey(sessionID)
 
 	mergeScript := redis.NewScript(`
 		local values = redis.call("HGETALL", KEYS[1])
@@ -82,4 +120,28 @@ func (s *SessionManager) Merge(oldSessionID, sessionID string) (int, error) {
 		return 0, err
 	}
 	return int(totalKeys.(int64)), nil
+}
+
+//Load loads a session from the storage with all its items
+func (s *SessionManager) Load(sessionID string) (*Session, error) {
+	all, err := s.client.HGetAll(getSessionKey(sessionID)).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	sess := &Session{
+		ID:      sessionID,
+		Manager: s,
+		data:    make(map[string]interface{}),
+	}
+
+	for k, v := range all {
+		item, err := sess.Deserialize(v)
+		if err != nil {
+			continue
+		}
+		sess.data[k] = item
+	}
+
+	return sess, nil
 }
--- extensions/session_manager_test.go
+++ extensions/session_manager_test.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 
 	"gopkg.in/redis.v4"
+	"gopkg.in/vmihailenco/msgpack.v2"
 
 	"github.com/heynemann/level/extensions"
 	"github.com/satori/go.uuid"
@@ -30,7 +31,7 @@ func getDefaultSM() *extensions.SessionManager {
 	return sessionManager
 }
 
-var _ = Describe("Session Manager", func() {
+var _ = Describe("Session Management", func() {
 
 	var testClient *redis.Client
 
@@ -42,109 +43,167 @@ var _ = Describe("Session Manager", func() {
 		})
 	})
 
-	Describe("Initialized properly", func() {
+	Describe("Session Manager", func() {
 
-		It("When getting a new instance", func() {
-			sessionManager, err := extensions.GetSessionManager(
-				"localhost", // Redis Host
-				7777,        // Redis Port
-				"",          // Redis Pass
-				0,           // Redis DB
-			)
+		Describe("Initialized properly", func() {
 
-			Expect(err).NotTo(HaveOccurred())
-			Expect(sessionManager).NotTo(BeNil())
+			It("When getting a new instance", func() {
+				sessionManager, err := extensions.GetSessionManager(
+					"localhost", // Redis Host
+					7777,        // Redis Port
+					"",          // Redis Pass
+					0,           // Redis DB
+				)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(sessionManager).NotTo(BeNil())
+			})
+
+			It("should be connected to Redis", func() {
+				_, err := extensions.GetSessionManager(
+					"localhost", // Redis Host
+					7777,        // Redis Port
+					"",          // Redis Pass
+					0,           // Redis DB
+				)
+
+				Expect(err).NotTo(HaveOccurred())
+			})
 		})
 
-		It("should be connected to Redis", func() {
-			_, err := extensions.GetSessionManager(
-				"localhost", // Redis Host
-				7777,        // Redis Port
-				"",          // Redis Pass
-				0,           // Redis DB
-			)
+		Describe("Initialized with wrong params", func() {
+			It("should not be connected to Redis", func() {
+				sessionManager, err := extensions.GetSessionManager(
+					"localhost", // Redis Host
+					1249,        // Redis Port
+					"",          // Redis Pass
+					0,           // Redis DB
+				)
 
-			Expect(err).NotTo(HaveOccurred())
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("connection refused"))
+				Expect(sessionManager).To(BeNil())
+			})
+		})
+
+		Describe("can start sessions", func() {
+			It("should start a session when provided with session id", func() {
+				sm := getDefaultSM()
+				sessionID := uuid.NewV4().String()
+				sm.Start(sessionID)
+
+				hashKey := fmt.Sprintf("session-%s", sessionID)
+				exists, err := testClient.Exists(hashKey).Result()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(exists).To(BeTrue())
+
+				lastUpdated, err := testClient.HGet(hashKey, "lastupdated").Result()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(lastUpdated).NotTo(BeNil())
+			})
+		})
+
+		Describe("can merge sessions", func() {
+			It("should merge a session into another one", func() {
+				sm := getDefaultSM()
+
+				oldSessionID := uuid.NewV4().String()
+				sm.Start(oldSessionID)
+				hashKey := fmt.Sprintf("session-%s", oldSessionID)
+				testClient.HSet(hashKey, "someKey", "someValue")
+
+				sessionID := uuid.NewV4().String()
+				sm.Start(sessionID)
+				count, err := sm.Merge(oldSessionID, sessionID)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(count).To(Equal(1))
+
+				exists, err := testClient.Exists(hashKey).Result()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(exists).To(BeFalse())
+
+				hashKey = fmt.Sprintf("session-%s", sessionID)
+				exists, err = testClient.Exists(hashKey).Result()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(exists).To(BeTrue())
+
+				someValue, err := testClient.HGet(hashKey, "someKey").Result()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(someValue).To(Equal("someValue"))
+			})
+
+			It("should not merge a non-existing session", func() {
+				sm := getDefaultSM()
+
+				sessionID := uuid.NewV4().String()
+				sm.Start(sessionID)
+
+				count, err := sm.Merge("invalid-id", sessionID)
+				Expect(count).To(Equal(0))
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal("Session with session ID invalid-id was not found in session storage."))
+			})
+		})
+
+		Describe("can get session", func() {
+			It("should be able to load a session", func() {
+				sm := getDefaultSM()
+				sessionID := uuid.NewV4().String()
+				sm.Start(sessionID)
+
+				session, err := sm.Load(sessionID)
+
+				Expect(err).ToNot(HaveOccurred())
+
+				Expect(session.ID).To(Equal(sessionID))
+				Expect(session.Manager).To(BeEquivalentTo(sm))
+				Expect(session.Get("lastupdated")).To(BeNumerically(">", 0))
+			})
 		})
 	})
 
-	Describe("Initialized with wrong params", func() {
-		It("should not be connected to Redis", func() {
-			sessionManager, err := extensions.GetSessionManager(
-				"localhost", // Redis Host
-				1249,        // Redis Port
-				"",          // Redis Pass
-				0,           // Redis DB
-			)
+	Describe("Session", func() {
+		Describe("can serialize", func() {
+			It("should serialize using msgpack", func() {
+				expected := map[string]interface{}{"a": 1}
+				s := extensions.Session{}
+				result, err := s.Serialize(expected)
 
-			Expect(err).NotTo(BeNil())
-			Expect(err.Error()).To(ContainSubstring("connection refused"))
-			Expect(sessionManager).To(BeNil())
-		})
-	})
+				Expect(err).NotTo(HaveOccurred())
 
-	Describe("can start sessions", func() {
-		It("should start a session when provided with session id", func() {
-			sm := getDefaultSM()
-			sessionID := uuid.NewV4().String()
-			sm.Start(sessionID)
+				serialized, err := msgpack.Marshal(expected)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(result).To(BeEquivalentTo(serialized))
+			})
 
-			hashKey := fmt.Sprintf("session-%s", sessionID)
-			exists, err := testClient.Exists(hashKey).Result()
-			Expect(err).NotTo(HaveOccurred())
-			Expect(exists).To(BeTrue())
-
-			lastUpdated, err := testClient.HGet(hashKey, "lastupdated").Result()
-			Expect(err).NotTo(HaveOccurred())
-			Expect(lastUpdated).NotTo(BeNil())
-		})
-	})
-
-	Describe("can merge sessions", func() {
-		It("should merge a session into another one", func() {
-			sm := getDefaultSM()
-
-			oldSessionID := uuid.NewV4().String()
-			sm.Start(oldSessionID)
-			hashKey := fmt.Sprintf("session-%s", oldSessionID)
-			testClient.HSet(hashKey, "someKey", "someValue")
-
-			sessionID := uuid.NewV4().String()
-			sm.Start(sessionID)
-			count, err := sm.Merge(oldSessionID, sessionID)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(count).To(Equal(1))
-
-			exists, err := testClient.Exists(hashKey).Result()
-			Expect(err).NotTo(HaveOccurred())
-			Expect(exists).To(BeFalse())
-
-			hashKey = fmt.Sprintf("session-%s", sessionID)
-			exists, err = testClient.Exists(hashKey).Result()
-			Expect(err).NotTo(HaveOccurred())
-			Expect(exists).To(BeTrue())
-
-			someValue, err := testClient.HGet(hashKey, "someKey").Result()
-			Expect(err).NotTo(HaveOccurred())
-			Expect(someValue).To(Equal("someValue"))
+			It("should fail to serialize invalid object", func() {
+				s := extensions.Session{}
+				_, err := s.Serialize(func() {})
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal("msgpack: Encode(unsupported func())"))
+			})
 		})
 
-		It("should not merge a non-existing session", func() {
-			sm := getDefaultSM()
+		Describe("can deserialize", func() {
+			It("should deserialize using msgpack", func() {
+				expected := map[interface{}]interface{}{"a": 1}
+				serialized, err := msgpack.Marshal(expected)
+				Expect(err).NotTo(HaveOccurred())
 
-			sessionID := uuid.NewV4().String()
-			sm.Start(sessionID)
+				s := extensions.Session{}
+				result, err := s.Deserialize(string(serialized))
+				actual := result.(map[interface{}]interface{})
 
-			count, err := sm.Merge("invalid-id", sessionID)
-			Expect(count).To(Equal(0))
-			Expect(err).NotTo(BeNil())
-			Expect(err.Error()).To(Equal("Session with session ID invalid-id was not found in session storage."))
-		})
-	})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(actual["a"]).To(BeEquivalentTo(1))
+			})
 
-	Describe("can set new keys", func() {
-		It("should be able to set a new key", func() {
-
+			It("should fail to deserialize invalid payload", func() {
+				s := extensions.Session{}
+				_, err := s.Deserialize("")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal("EOF"))
+			})
 		})
 	})
 })
