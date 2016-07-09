f345ea9ffdd4bf682b566ca737595b46de998374
Bernardo Heynemann <heynemann@gmail.com>
2016-07-08 23:45:04 -0300

    Logging and logging testing

--- extensions/heartbeat/heartbeat.go
+++ extensions/heartbeat/heartbeat.go
@@ -11,6 +11,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/uber-go/zap"
+
 	"gopkg.in/redis.v4"
 )
 
@@ -18,15 +20,36 @@ import (
 type Heartbeat struct {
 	ServerID           string
 	RegistryExpiration time.Duration
+	UpdateInterval     time.Duration
+	Logger             zap.Logger
 	client             *redis.Client
 }
 
+//NewDefault returns a new instance of the heartbeat extension with default options
+func NewDefault(serverID string, redisHost string, redisPort int, redisPass string, redisDB int, logger zap.Logger) (*Heartbeat, error) {
+	return New(serverID, redisHost, redisPort, redisPass, redisDB, logger, 3*time.Minute, 10*time.Second)
+}
+
 //New creates a new instance of the Heartbeat extension
-func New(serverID, redisHost string, redisPort int, redisPass string, redisDB int, registryExpiration time.Duration) (*Heartbeat, error) {
+func New(serverID, redisHost string, redisPort int, redisPass string, redisDB int, logger zap.Logger, registryExpiration, updateInterval time.Duration) (*Heartbeat, error) {
+	l := logger.With(
+		zap.String("source", "heartbeat"),
+		zap.Duration("expiration", registryExpiration),
+		zap.Duration("interval", updateInterval),
+	)
 	h := Heartbeat{
 		ServerID:           serverID,
 		RegistryExpiration: registryExpiration,
+		UpdateInterval:     updateInterval,
+		Logger:             l,
 	}
+	rl := l.With(
+		zap.String("redisHost", redisHost),
+		zap.Int("redisPort", redisPort),
+		zap.Int("redisDB", redisDB),
+	)
+
+	rl.Debug("Connecting to Redis...")
 	h.client = redis.NewClient(&redis.Options{
 		Addr:     fmt.Sprintf("%s:%d", redisHost, redisPort),
 		Password: redisPass,
@@ -35,8 +58,11 @@ func New(serverID, redisHost string, redisPort int, redisPass string, redisDB in
 
 	_, err := h.client.Ping().Result()
 	if err != nil {
+		rl.Error("Could not connect to redis.", zap.Error(err))
 		return nil, err
 	}
+
+	rl.Info("Connected to Redis successfully.")
 
 	return &h, nil
 }
@@ -65,12 +91,12 @@ func (h *Heartbeat) Register() error {
 }
 
 //Start the server heartbeat
-func (h *Heartbeat) Start() *chan bool {
+func (h *Heartbeat) Start() chan bool {
 	done := make(chan bool)
 
 	go func(self *Heartbeat) {
 		for {
-			switch {
+			select {
 			case <-done:
 				return
 			default:
@@ -79,10 +105,10 @@ func (h *Heartbeat) Start() *chan bool {
 					fmt.Println("Could not submit status to redis. Will retry in 10 seconds.", err)
 				}
 				fmt.Println("Status updated successfully in redis. Sleeping for 10 seconds...")
-				time.Sleep(10 * time.Second)
+				time.Sleep(self.UpdateInterval)
 			}
 		}
 	}(h)
 
-	return &done
+	return done
 }
--- extensions/heartbeat/heartbeat_test.go
+++ extensions/heartbeat/heartbeat_test.go
@@ -8,54 +8,99 @@
 package heartbeat_test
 
 import (
+	"fmt"
 	"strconv"
 	"time"
 
 	"gopkg.in/redis.v4"
 
 	"github.com/heynemann/level/extensions/heartbeat"
+	. "github.com/heynemann/level/testing"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/uber-go/zap"
 )
+
+func verifyServerRegistered(cli *redis.Client, serverName string) {
+	serverKey := fmt.Sprintf("server-status:%s", serverName)
+	result, err := cli.Get(serverKey).Result()
+	Expect(err).NotTo(HaveOccurred())
+	value, err := strconv.ParseInt(result, 10, 64)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(int(value)).To(BeNumerically(">", 0))
+
+	list, err := cli.SMembers("available-servers").Result()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(list[0]).To(Equal(serverName))
+}
 
 var _ = Describe("Heartbeat", func() {
 	var testClient *redis.Client
+	var logger *MockLogger
 
 	BeforeEach(func() {
+		logger = NewMockLogger()
 		testClient = redis.NewClient(&redis.Options{
 			Addr:     "localhost:7777",
 			Password: "",
 			DB:       0,
 		})
+
+		_, err := testClient.FlushAll().Result()
+		Expect(err).NotTo(HaveOccurred())
 	})
 
 	Describe("Heartbeat Extension", func() {
 		Describe("Heartbeat creation", func() {
 			It("should create new heartbeat", func() {
-				heartbeat, err := heartbeat.New("some-server", "localhost", 7777, "", 0, 10*time.Second)
+				heartbeat, err := heartbeat.NewDefault("other-server", "localhost", 7777, "", 0, logger)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(heartbeat).NotTo(BeNil())
+				Expect(logger.Messages).To(HaveLen(2))
+
+				Expect(logger).To(HaveLogMessage(
+					zap.DebugLevel, "Connecting to Redis...",
+					"source", "heartbeat",
+					"redisHost", "localhost",
+					"redisPort", 7777,
+					"redisDB", 0,
+				))
+
+				Expect(logger).To(HaveLogMessage(
+					zap.InfoLevel, "Connected to Redis successfully.",
+					"source", "heartbeat",
+					"redisHost", "localhost",
+					"redisPort", 7777,
+					"redisDB", 0,
+				))
 			})
 		})
 
 		Describe("Heartbeat registry", func() {
 			It("should register server in redis", func() {
-				heartbeat, err := heartbeat.New("some-server", "localhost", 7777, "", 0, 10*time.Second)
+				heartbeat, err := heartbeat.NewDefault("other-server", "localhost", 7777, "", 0, logger)
 				Expect(err).NotTo(HaveOccurred())
 
 				err = heartbeat.Register()
 				Expect(err).NotTo(HaveOccurred())
 
-				serverKey := "server-status:some-server"
-				result, err := testClient.Get(serverKey).Result()
-				Expect(err).NotTo(HaveOccurred())
-				value, err := strconv.ParseInt(result, 10, 64)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(int(value)).To(BeNumerically(">", 0))
+				verifyServerRegistered(testClient, "other-server")
+			})
+		})
 
-				list, err := testClient.SMembers("available-servers").Result()
+		Describe("Heartbeat Started", func() {
+			It("should register server with start", func() {
+				heartbeat, err := heartbeat.New("some-other-server", "localhost", 7777, "", 0, logger, 10*time.Second, 10*time.Millisecond)
 				Expect(err).NotTo(HaveOccurred())
-				Expect(list[0]).To(Equal("some-server"))
+
+				done := heartbeat.Start()
+				time.Sleep(time.Millisecond)
+				defer func(close chan bool) {
+					close <- true
+				}(done)
+
+				Expect(err).NotTo(HaveOccurred())
+				verifyServerRegistered(testClient, "some-other-server")
 			})
 		})
 	})
