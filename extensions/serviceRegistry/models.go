a647464fc735ad9b7db99238a75b3219a107f3d5
Bernardo Heynemann <heynemann@gmail.com>
2016-12-28 21:42:43 -0200

    starting over

--- extensions/serviceRegistry/models.go
+++
@@ -1,8 +0,0 @@
-// level game server
-// https://github.com/heynemann/level
-//
-// Licensed under the MIT license:
-// http://www.opensource.org/licenses/mit-license
-// Copyright © 2016 Bernardo Heynemann <heynemann@gmail.com>
-
-package registry
